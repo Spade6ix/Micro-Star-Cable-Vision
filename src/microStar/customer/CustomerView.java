9946c92b592f84326fafebbf8f5082ba217e1571
Alex Allen <alexallen1016@gmail.com>
2022-03-08 11:41:36 -0800

    .

--- src/microStar/customer/CustomerView.java
+++ src/microStar/customer/CustomerView.java
@@ -1,4 +1,19 @@
 package microStar.customer;
 
+import microStar.login.LoginScreenView;
+
 public class CustomerView {
+	
+	
+	public static void createCustomerLoginScreen() {
+		LoginScreenView loginScreen = new LoginScreenView();
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		CustomerView.createCustomerLoginScreen();
+	}
+
+	
 }
