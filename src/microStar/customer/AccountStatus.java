ba722104a6238e2bae044d3d0096a040cf0937da
Sonneil Wellington <sonneil.wellington@gmail.com>
2022-03-16 17:21:00 -0500

    Bug Fix
    -Incorrect import statement fixed

--- src/microStar/customer/AccountStatus.java
+++ src/microStar/customer/AccountStatus.java
@@ -1,14 +1,12 @@
-package microstar.customer;
+package microStar.customer;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
-     import javax.swing.JPanel;
+import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.Color;
-import javax.swing.JLabel;
 import java.awt.Font;
-import javax.swing.JTextField;
 
 
 public class AccountStatus extends JFrame {
