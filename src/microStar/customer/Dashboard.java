afc75845df80977a3824e60b56a2c3361d1ef76c
Alex Allen <alexallen1016@gmail.com>
2022-04-05 13:07:02 -0700

    tool tips & mnemonics

--- src/microStar/customer/Dashboard.java
+++ src/microStar/customer/Dashboard.java
@@ -33,12 +33,12 @@ public class Dashboard extends JFrame implements MouseListener {
 	private JMenu view = new JMenu("View");
 	private JMenu other = new JMenu("Other");
 	private JMenu gap = new JMenu();
-	public JMenuItem  lodgeComplaint = new JMenuItem("Logde Complaint");
-	public JMenuItem accountStatus = new JMenuItem("Account Status");
-	public JMenuItem paymentHistory = new JMenuItem("Payment History");
-	public JMenuItem complaintHistory = new JMenuItem("Complaint History");
-	public JMenuItem liveChat = new JMenuItem("Live Chat");
-	public JMenuItem liveVideoChat = new JMenuItem("Live Video Chat");
+	public JMenuItem  lodgeComplaint = new JMenuItem("Logde Complaint  'l'");
+	public JMenuItem accountStatus = new JMenuItem("Account Status  'a'");
+	public JMenuItem paymentHistory = new JMenuItem("Payment History  'p'");
+	public JMenuItem complaintHistory = new JMenuItem("Complaint History  'c'");
+	public JMenuItem liveChat = new JMenuItem("Live Chat  't'");
+	public JMenuItem liveVideoChat = new JMenuItem("Live Video Chat  'v'");
 	
 	
 	
@@ -54,24 +54,36 @@ public class Dashboard extends JFrame implements MouseListener {
         
         //Services Menu
         lodgeComplaint.setFont(new Font("Calibri", Font.PLAIN, 15));
+        lodgeComplaint.setToolTipText("Report issues/complaints");
+        lodgeComplaint.setMnemonic('l');
         services.add(lodgeComplaint);
         accountStatus.setFont(new Font("Calibri", Font.PLAIN, 15));
+        accountStatus.setToolTipText("Get the status of you account");
+        accountStatus.setMnemonic('a');
         services.add(accountStatus);
         services.setPreferredSize(new Dimension(100, 0)); 
         services.setForeground(new Color(0xffffff));
         
         //View Menu
         paymentHistory.setFont(new Font("Calibri", Font.PLAIN, 15));
+        paymentHistory.setToolTipText("View past payments");
+        paymentHistory.setMnemonic('p');
         view.add(paymentHistory);
         complaintHistory.setFont(new Font("Calibri", Font.PLAIN, 15));
+        complaintHistory.setToolTipText("View all past complaints");
+        complaintHistory.setMnemonic('c');
         view.add(complaintHistory);
         view.setPreferredSize(new Dimension(100, 0));
         view.setForeground(new Color(0xffffff));
         
         //Other Menu
         liveChat.setFont(new Font("Calibri", Font.PLAIN, 15));
+        liveChat.setToolTipText("Live text chat");
+        liveChat.setMnemonic('t');
         other.add(liveChat);
         liveVideoChat.setFont(new Font("Calibri", Font.PLAIN, 15));
+        liveVideoChat.setToolTipText("Live video chat");
+        liveVideoChat.setMnemonic('v');
         other.add(liveVideoChat);
         other.setPreferredSize(new Dimension(100, 0));
         other.setForeground(new Color(0xffffff));
